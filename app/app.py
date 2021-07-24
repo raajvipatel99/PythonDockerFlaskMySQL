61526732f33fa707e70de836b45989dffe640724
raajvipatel99 <raajvipatel99@gmail.com>
2021-07-23 23:52:18 -0400

    Task: code changes

--- app/app.py
+++ app/app.py
@@ -53,6 +53,7 @@ def form_update_post(city_id):
     mysql.get_db().commit()
     return redirect("/", code=302)
 
+
 @app.route('/cities/new', methods=['GET'])
 def form_insert_get():
     return render_template('new.html', title='New City Form')
@@ -68,6 +69,7 @@ def form_insert_post():
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
+
 
 @app.route('/delete/<int:city_id>', methods=['POST'])
 def form_delete_post(city_id):
@@ -98,21 +100,65 @@ def api_retrieve(city_id) -> str:
     return resp
 
 
-@app.route('/api/v1/cities/', methods=['POST'])
+# @app.route('/api/v1/cities', methods=['POST'])
+# def api_add() -> str:
+#     cursor = mysql.get_db().cursor()
+#     inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
+#                  request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
+#                  request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'))
+#     sql_insert_query = """INSERT INTO tblCitiesImport (fldName,fldLat,fldLong,fldCountry,fldAbbreviation,fldCapitalStatus,fldPopulation) VALUES (%s, %s,%s, %s,%s, %s,%s) """
+#     cursor.execute(sql_insert_query, inputData)
+#     mysql.get_db().commit()
+#     return redirect("/", code=302)
+
+@app.route('/api/v1/cities', methods=['POST'])
 def api_add() -> str:
+    content = request.json
+    cursor = mysql.get_db().cursor()
+    inputData = (content['fldName'], content['fldLat'], content['fldLong'],
+                 content['fldCountry'], content['fldAbbreviation'],
+                 content['fldCapitalStatus'], content['fldPopulation'])
+    sql_insert_query = """INSERT INTO tblCitiesImport (fldName,fldLat,fldLong,fldCountry,fldAbbreviation,fldCapitalStatus,fldPopulation) VALUES (%s, %s,%s, %s,%s, %s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
     resp = Response(status=201, mimetype='application/json')
     return resp
 
+
+# @app.route('/api/v1/cities/<int:city_id>', methods=['PUT'])
+# def api_edit(city_id) -> str:
+#     cursor = mysql.get_db().cursor()
+#     inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
+#                  request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
+#                  request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'))
+#     sql_update_query = """UPDATE tblCitiesImport t SET t.fldName = %s, t.fldLat = %s, t.fldLong = %s, t.fldCountry =
+#     %s, t.fldAbbreviation = %s, t.fldCapitalStatus = %s, t.fldPopulation = %s WHERE t.id = %s """
+#     cursor.execute(sql_update_query, inputData)
+#     mysql.get_db().commit()
+#     return redirect("/", code=302)
 
 @app.route('/api/v1/cities/<int:city_id>', methods=['PUT'])
 def api_edit(city_id) -> str:
-    resp = Response(status=201, mimetype='application/json')
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    inputData = (content['fldName'], content['fldLat'], content['fldLong'],
+                 content['fldCountry'], content['fldAbbreviation'],
+                 content['fldCapitalStatus'], content['fldPopulation'], city_id)
+    sql_update_query = """UPDATE tblCitiesImport t SET t.fldName = %s, t.fldLat = %s, t.fldLong = %s, t.fldCountry = 
+        %s, t.fldAbbreviation = %s, t.fldCapitalStatus = %s, t.fldPopulation = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/cities/<int:city_id>', methods=['DELETE'])
+@app.route('/api/v1/cities/<int:city_id>', methods=['DELETE'])
 def api_delete(city_id) -> str:
-    resp = Response(status=210, mimetype='application/json')
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM tblCitiesImport WHERE id = %s """
+    cursor.execute(sql_delete_query, city_id)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
     return resp
 
 
